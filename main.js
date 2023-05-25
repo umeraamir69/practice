35825e2f0df74de4953ea7a45d96999b401f48d3
Muhammad Umer Aamir <93091750+umeraamir69@users.noreply.github.com>
2023-05-25 23:25:42 +0500

    25 may 23

---
+++ main.js
@@ -0,0 +1,99 @@
+const aasfd = "sadfa";  //same value   || define
+let abc = "sfsd"    //different value
+
+
+
+let asd = 5
+let dsa = "umer"
+let ali = 2342.23423;
+let c = ["adsfas", 3423, "sfsd", "dfs", 345]
+
+
+
+
+pass = "1233.."
+
+
+
+
+
+//array methood 
+
+//add
+c.push("umer")
+//delete
+// c.pop()
+//splice
+// c.splice(0, 3)
+//
+// c[2] = "umer"
+//
+
+
+
+console.log("haris")
+
+
+
+
+
+
+// 
+
+
+
+
+let studnets = ["haris", 112, 5343, "umer", 243, 23]
+
+
+let as = [
+    { name: "haris", roll: 114, grade: 3.0 },
+    { name: "hassan", roll: 69, grade: 3.7 }
+]
+
+
+
+for (let index = 0; index < as.length; index++) {
+    console.log(as[index])
+}
+
+
+
+
+let s = { name: "hassan", roll: 69, grade: 3.7 }
+console.log(JSON.stringify(s))
+
+let sa = '{"name":"hassan","roll":69,"grade":3.7}'
+console.log("=-----parse----");
+console.log(JSON.parse(sa))
+
+let a = "456";
+
+if (a == 123) {
+    console.log("123 is presnet ")
+}
+else if (a === 456) {
+    console.log(4506)
+}
+else {
+    console.log(false)
+}
+
+
+
+
+
+
+//  attach JS with HTML
+//  JSON (Parse , stringify)
+//  variable (let ,const)
+// array
+// for loop
+// console statments
+// If else
+// comparison opertor  === type and value  , == value 
+
+
+
+// functions
+// arrow functions
